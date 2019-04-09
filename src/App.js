42afebaa456159ee9eb2e24e9020eafefd8c8b9d
Ljuba Beglerbegovic <ljuba93b@gmail.com>
2019-04-09 12:19:59 -0700

    Show all posts on startup

--- src/App.js
+++ src/App.js
@@ -3,22 +3,36 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    isLoading: true,
+    posts: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch('/forum/posts');
+    const body = await response.json();
+    this.setState({ posts: body, isLoading: false });
+  }
+
   render() {
+    const {posts, isLoading} = this.state;
+
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div className="App-intro">
+            <h2>Posts</h2>
+            {posts.map(post =>
+              <div key={post.id}>
+                {post.subject}
+              </div>
+            )}
+          </div>
         </header>
       </div>
     );
